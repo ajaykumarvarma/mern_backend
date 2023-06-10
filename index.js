769877258a6549e132b64d83171eba69b25ea183
N.AJAY KUMAR VARMA <90514443+ajaykumarvarma@users.noreply.github.com>
2023-06-10 15:54:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express")
+const app = express()
+const products = require("./products")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const bodyParser = require("body-parser")
+const Content = require("./schema")
+
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
+app.use(cors())
+app.use(bodyParser.json())
+mongoose.connect("mongodb+srv://ajaynagaraju32:ajaynag833@codegnan.isbtslh.mongodb.net/firstdb?retryWrites=true&w=majority")
+    .then(() => {
+        console.log("MongoDB Connected Successfully")
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+
+app.post("/add",(req,res) => {
+ const {name,passcode} = req.body
+  const newData = new Content({
+    name,passcode
+  })
+  newData.save()
+  res.send("Successfully added into database")
+
+})
+
+app.get("/retrieve",(req,res)=>{
+    Content.find()
+    .then(found=>res.json(found))
+})
+app.get("/products",(req,res)=>{
+    res.json(products)
+})
+app.get("/", (req, res) => {
+
+    res.send("Server Started Successfully")
+
+})
+
+app.get("/name",(req,res)=>{
+    res.send("Ajay IT Solutions")
+})
+
+app.listen(4000, () => {
+    console.log("Server Started Successfully")
+})
---
+++ products.js
@@ -0,0 +1,7 @@
+const productList = [
+    {id:1,name:"ajay",age:18,gender:"male"},
+    {id:2,name:"sai",age:20,gender:"male"},
+    {id:3,name:"charan",age:19,gender:"male"},
+    {id:4,name:"rizwan",age:18,gender:"male"}
+]
+module.exports = productList
